87334c990b2ebda90b215d15370060e78583be3f
juan pablo <juanpablo_1810@hotmail.com>
2022-10-11 21:56:26 -0500

    Puntos

---
+++ taller3.java
@@ -0,0 +1,27 @@
+/*
+ * 1. Elabore un algoritmo que permita averiguar cuál es el nombre del mayor de
+ * 2 hermanos no gemelos. Como datos de entrada se tiene el nombre y la edad de
+ * las 2 personas.
+ * 
+ * 2. Elabore un algoritmo que lea el nombre, el salario bruto, las deducciones
+ * y las bonificaciones de dos trabajadores, e imprima el nombre del que más
+ * salario neto tiene.
+ * 
+ * 3. Crear un algoritmo que le permita al usuario ingresar los datos de dos
+ * buses así: Placa, El número de pasajeros transportado y el valor del pasaje,
+ * y el computador le muestre la placa del bus que más dinero recogió.
+ * 
+ * 4. Elaborar un algoritmo donde el usuario ingrese la placa de un bus, el
+ * número de pasajeros transportados y la ruta donde prestó el servicio ( A o B)
+ * el computador le debe mostrar el dinero que recolectó sabiendo que en la ruta
+ * A el pasaje es a $1.200.oo y en la B a $1.000.oo
+ * 
+ * 5. Crear un algoritmo que le permita al usuario ingresar el tipo de
+ * trabajador (FIJO o TEMPORAL) y con base en esto pueda imprimir el nombre y el
+ * salario neto, sabiendo que si es FIJO debe leer el nombre, el número de horas
+ * trabajadas, el salario básico hora,
+ * el total de deducciones y el total de bonificaciones y si es TEMPORAL solo
+ * debe leer el nombre y el número de horas trabajadas; estos trabajadores
+ * tienen un salario básico hora fijo de $6.000.oo y no tienen deducciones ni
+ * bonificaciones.
+ */
